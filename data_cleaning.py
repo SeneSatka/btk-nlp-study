c8b4cc5149e566c0139aa8190381bfa206fec282
SeneSatka <senesatka@gmail.com>
2025-01-22 13:03:45 +0300

    Refactor data cleaning function to improve text processing and remove stop words

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,13 +2,19 @@ from bs4 import BeautifulSoup
 from textblob import TextBlob
 import re
 import string
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
 
-def data_cleaning(text):
+def clean_text(text):
     nt=text.lower()
-    soup = BeautifulSoup(text, 'html.parser')
+    soup = BeautifulSoup(nt, 'html.parser')
     nt = soup.get_text()
+    nt =re.sub(r"\d+", "", nt)
     nt = re.sub(r'[^a-zA-Z0-9\s]', '', nt)
     nt = nt.translate(str.maketrans('', '', string.punctuation))
     nt = TextBlob(nt).correct()
+    nt = " ".join([word for word in nt.split() if len(word)>2 and word not in stop_words])
     return nt
     
