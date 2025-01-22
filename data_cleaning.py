988239e7f809a8fa0ccd66fbcd95d1dd084da777
SeneSatka <senesatka@gmail.com>
2025-01-22 14:04:55 +0300

    Enhance clean_text function to support HTML parsing and text correction options

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,14 +7,15 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
-def clean_text(text):
+def clean_text(text,isHtml=False,correct_text=False):
     nt=text.lower()
-    soup = BeautifulSoup(nt, 'html.parser')
-    nt = soup.get_text()
-    nt =re.sub(r"\d+", "", nt)
-    nt = re.sub(r'[^a-zA-Z0-9\s]', '', nt)
+    if isHtml:
+        soup = BeautifulSoup(nt, 'html.parser')
+        nt = soup.get_text()
+    nt = re.sub(r'[^a-zA-Z\s]', '', nt)
     nt = nt.translate(str.maketrans('', '', string.punctuation))
-    nt = TextBlob(nt).correct()
+    if correct_text:
+        nt = TextBlob(nt).correct()
     nt = " ".join([word for word in nt.split() if len(word)>2 and word not in stop_words])
     return nt
     
