059393f4ae377c203c8b44889ae0f71325129c32
SeneSatka <senesatka@gmail.com>
2025-01-24 22:10:30 +0300

    Add Word2Vec model implementation with KMeans clustering and PCA visualization

--- word_embedding_imdb_dataset.py
+++ word_embedding_imdb_dataset.py
@@ -19,3 +19,34 @@ cleaned_docs=[clean_text(sentence) for sentence in docs]
 tokenized_docs=[simple_preprocess(doc) for doc in cleaned_docs]
 
 
+# word2vec model tanımlama
+model=Word2Vec(sentences=tokenized_docs,vector_size=50,window=5,min_count=1,sg=0)
+word_vectors=model.wv
+
+words = list(word_vectors.index_to_key)
+vectors = [word_vectors[word] for word in words]
+
+#clustering KMeans K=2
+kmeans=KMeans(n_clusters=4)
+kmeans.fit(vectors)
+clusters=kmeans.labels_
+
+# PCA 50 -> 2
+pca=PCA(n_components=2)
+reduced_vectors=pca.fit_transform(vectors)
+
+
+# 2 boyutlu görselleştirme
+plt.figure()
+plt.scatter(reduced_vectors[:,0],reduced_vectors[:,1],c=clusters,cmap="viridis")
+
+centers= pca.transform(kmeans.cluster_centers_)
+plt.scatter(centers[:,0],centers[:,1],c="red",marker="x",s=170,label="Center")
+plt.legend()
+
+for i,word in enumerate(words):
+    plt.text(reduced_vectors[i,0],reduced_vectors[i,1],word,fontsize=7)
+
+
+
+plt.show()
